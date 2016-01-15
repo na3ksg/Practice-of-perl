d16874b6a26e4d842a43cf55afc5b19323ba314e
na3ksg <superkasago@yahoo.co.jp>
2016-01-15 23:44:22 +0900

    英語と数学の合計点を求めるスクリプトの例

--- sum.pl
+++ sum.pl
@@ -25,3 +25,18 @@ sub sum{
 	# 戻り値
 	return $total;
 }
+
+# 変数の宣言と代入：英語と数学の点数
+my $score_math = 77;
+my $score_english = 80;
+
+# 四則演算：英語と数学の点数の足し算
+my $score_total = $score_english + $score_math;
+
+# 文字列：計算結果を見やすい形に編集
+my $score_list = "Math : $score_math\n";.
+				 "English : $score_english\n";.
+				 "Total : $score_total\n";
+
+# print関数：計算結果の出力
+print $score_list;
