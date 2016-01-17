2b1ad9c9b818e44c6bcd0a4eab6bad373ba906ab
na3ksg <superkasago@yahoo.co.jp>
2016-01-17 23:22:57 +0900

    各教科の点数を評価するスクリプトの例

--- sum.pl
+++ sum.pl
@@ -52,3 +52,20 @@ foreach my $num(@nums){
 
 # 和の出力
 print "Total:$total\n";
+
+# ハッシュの宣言と代入：各教科の点数
+my %scores = (math => 55,english => 70,japanese => 100);
+
+# 試験結果の表示
+foreach my $subject (keys %scores){
+	# 各教科の点数
+	my $score = $scores {$subject};
+	# 制御構文：80点以上はgood
+	if ($score >= 80) {
+		print "$subject : good\n";
+	} elsif ($score >= 60){
+		print "$subject : pass\n";
+	} else {
+		print "$subject : failed\n";
+	}
+}
